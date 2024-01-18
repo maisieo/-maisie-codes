39672c173bdf063d1ba5e81ac67757a01bea8bdc
Maisie <maisieobb@googlemail.com>
2024-01-18 09:03:55 +0000

    correct font awesome, add in social icons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,14 +4,21 @@ import './Header.scss';
 function Header() {
   return (
     <header className="header">
-      <div className="logo">
+
+      <p className="name">Maisie O'Brien-Butcher </p> 
+
+      <div className="social-icons">
+        {/* Social Icons */}
+        <a href="https://github.com/maisieo" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github"></i>
+        </a>
+        <a href="https://linkedin.com/in/maisie-obb" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-linkedin"></i>
+        </a>
+        <a href="mailto:maisieobb@gmail.com">
+          <i className="fas fa-envelope"></i>
+        </a>
       </div>
-      <nav className="navigation">
-        {/* Navigation links */}
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
-      </nav>
     </header>
   );
 }
