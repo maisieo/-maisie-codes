1f2cc57e46bc3af9cd81188b40c591eaa1b58ab2
Maisie <maisieobb@googlemail.com>
2024-01-19 11:56:12 +0000

    button to a

--- src/components/IntroSection/IntroSection.js
+++ src/components/IntroSection/IntroSection.js
@@ -8,7 +8,7 @@ function IntroSection() {
         <h1 className="header-text">I'm Maisie!
         A Frontend Developer</h1>
         <p className="sub-heading">FE developer with experience building pixel-perfect front ends with Javascript libraries such as React, LWC, and with other technologies and languages like HTML, CSS, Figma, Litmus.</p>
-        <button className="contact-button">Get in touch</button>
+        <a className="contact-button">Get in touch</a>
       </div>
       <div className="profile-img">
        
