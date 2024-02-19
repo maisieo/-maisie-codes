27c4af535763b03545d74fac768841a01d3dd377
Maisie <maisieobb@googlemail.com>
2024-02-19 13:15:36 +0000

    Skill update

--- src/components/SkillSection/SkillSection.js
+++ src/components/SkillSection/SkillSection.js
@@ -3,15 +3,21 @@ import './SkillSection.scss';
 
 
 let skills = [
-    { title: 'HMTL', description: '3 years' },
-    { title: 'CSS', description: 'D3 years' },
+    { title: 'HTML', description: '3 years' },
+    { title: 'CSS', description: '3 years' },
+    { title: 'Adobe XD', description: 'Experience' },
+    { title: 'Figma', description: 'Experience' },
+    { title: 'Email Builds', description: 'Experience' },
+    { title: 'React', description: 'Experience' },
+    { title: 'Salesforce', description: 'Experience' }
+];
 
-]
 function SkillSection() {
   return (
     <section className="skill-section">
+
       {skills.map((skill, index) => (
-        <div key={index} className={skill}>
+        <div key={index} className='skill'>
           <h1>{skill.title}</h1>
           <p>{skill.description}</p>
         </div>
