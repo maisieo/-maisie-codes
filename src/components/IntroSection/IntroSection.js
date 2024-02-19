890ad76da5b6ad51112380bc6a6b35b46713f07e
Maisie <maisieobb@googlemail.com>
2024-02-19 12:42:09 +0000

    underline to maisie

--- src/components/IntroSection/IntroSection.js
+++ src/components/IntroSection/IntroSection.js
@@ -5,10 +5,10 @@ function IntroSection() {
   return (
     <section className="intro-section">
       <div className="text-content">
-        <h1 className="header-text">I'm Maisie!
+        <h1 className="header-text">I'm <span className="underline">Maisie!</span><br />
         A Frontend Developer</h1>
         <p className="sub-heading">FE developer with experience building pixel-perfect front ends with Javascript libraries such as React, LWC, and with other technologies and languages like HTML, CSS, Figma, Litmus.</p>
-        <a className="contact-button">Get in touch</a>
+        <a className="contact-button" href='google.com'>Get in touch</a>
       </div>
       <div className="profile-img">
        
