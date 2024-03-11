de77ed114e3cc78036306d96615ac2da8abc740d
Maisie <maisieobb@googlemail.com>
2024-03-11 12:20:42 +0000

    change form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -47,58 +47,55 @@ function ContactForm() {
   return (
     <div className="contact-section">
       <div className="inner-wrapper">
-      <div className="contact-info">
-        <h2>Contact</h2>
-        <p>
-          Feel free to reach out to me with any questions. I'm open to hearing
-          about roles or projects you're currently working on.
-        </p>
-      </div>
-      <div className="contact-form">
-        <form
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          onSubmit={handleSubmit}
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <div className="input-section">
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="input-section">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              name="email"
-              id="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="input-section">
-            <label htmlFor="message">Message</label>
-            <textarea
-              name="message"
-              id="message"
-              value={formData.message}
-              onChange={handleChange}
-              required
-            ></textarea>
-          </div>
-          <div className="button-container">
-            <button type="submit" className="contact-button">Send message</button>
-          </div>
-        </form>
-      </div>
+        <div className="contact-info">
+          <h2>Contact</h2>
+          <p>
+            Feel free to reach out to me with any questions. I'm open to hearing
+            about roles or projects you're currently working on.
+          </p>
+        </div>
+        <div className="contact-form">
+          <form
+            name="contact"
+            method="POST"
+            data-netlify="true"
+            onSubmit={handleSubmit}
+          >
+            <input type="hidden" name="form-name" value="contact" />
+            <div className="input-section">
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="input-section">
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="input-section">
+              <textarea
+                name="message"
+                placeholder="Message"
+                value={formData.message}
+                onChange={handleChange}
+                required
+              ></textarea>
+            </div>
+            <div className="button-container">
+              <button type="submit" className="contact-button">Send message</button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
