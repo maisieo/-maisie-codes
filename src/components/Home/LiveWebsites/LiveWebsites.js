7acf8210cfb50a4456fed83f816d0e51bb78e9da
Maisie <maisieobb@googlemail.com>
2024-03-15 11:52:49 +0000

    rename images

--- src/components/Home/LiveWebsites/LiveWebsites.js
+++ src/components/Home/LiveWebsites/LiveWebsites.js
@@ -6,11 +6,29 @@ import ContactForm from "../ContactForm/ContactForm";
 function LiveWebsites() {
   return (
     <div>
-    <Header/>
-    <ContactForm/>
-    <Footer/>
-    </div>
+      <Header />
 
+      {/* First Section */}
+      <section>
+        <h2>Section 1</h2>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <img src="/assets/imgs/" alt="Website 1" />
+          <img src="image2.jpg" alt="Website 2" />
+        </div>
+      </section>
+
+      {/* Second Section */}
+      <section>
+        <h2>Section 2</h2>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <img src="image3.jpg" alt="Website 3" />
+          <img src="image4.jpg" alt="Website 4" />
+        </div>
+      </section>
+
+      <ContactForm />
+      <Footer />
+    </div>
   );
 }
 
