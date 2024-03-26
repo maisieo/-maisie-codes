244b7260108dd9bd97620a8f35fe3017aacb4c2c
Maisie <maisieobb@googlemail.com>
2024-03-26 21:56:12 +0000

    CSS tweaks

--- src/components/Home/EmailBuilds/EmailBuilds.js
+++ src/components/Home/EmailBuilds/EmailBuilds.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import ContactForm from "../ContactForm/ContactForm";
@@ -6,28 +6,55 @@ import { Link } from "react-router-dom";
 
 import './EmailBuilds.css';
 
-function EmailBuilds() {
+
+let EmailBuilds = () => {
+  let [current, setCurrent] = useState(0);
+  let images = [
+  "https://i.ibb.co/r6zx1yb/Email1.jpg",
+  "https://i.ibb.co/RBQMN8h/Email2.jpg",
+  "https://i.ibb.co/rM41z76/Email3.jpg",
+  "https://i.ibb.co/Cmxsc97/Email4.jpg"
+  ];
+
+  let nextImage = () =>
+    setCurrent(current === images.length - 1 ? 0 : current + 1);
+  let prevImage = () =>
+    setCurrent(current === 0 ? images.length - 1 : current - 1);
+  let jumpImage = (index) => setCurrent(index);
+
   return (
     <div>
-      <Header />
+    <Header />
       <div className='back-link'>
         <Link to='/'>Back</Link>
       </div>
-      {/* First Section */}
-      <section>
-        <h2>Section 1</h2>
-        <div className="image-container">
-          <img src="/assets/imgs/Emails/Email1.jpg" alt="Website 1" />
-          <img src="/assets/imgs/Emails/Email2.jpg" alt="Website 1" />
-          <img src="/assets/imgs/Emails/Email3.jpg" alt="Website 1" />
-          <img src="/assets/imgs/Emails/Email4.jpg" alt="Website 1" />
-
-        </div>
-      </section>
-      <ContactForm />
-      <Footer />
+    <div className="gallery-container">
+      <span className="button-prev" onClick={prevImage}>
+        chevron_left
+      </span>
+      <span className="button-next" onClick={nextImage}>
+        chevron_right
+      </span>
+      <div className="gallery-track">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className="slide-image"
+            style={{
+              backgroundImage: `url(${image})`,
+              transform: `translateX(${(index - current) * 100}%)`,
+            }}
+          />
+        ))}
+      </div>
+    </div>
+    <ContactForm/>
+    <Footer/>
     </div>
   );
-}
+};
+
 
 export default EmailBuilds;
+
+
--- src/components/Home/LiveWebsites/LiveWebsites.js
+++ src/components/Home/LiveWebsites/LiveWebsites.js
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
-import "./LiveWebsites.css";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import ContactForm from "../ContactForm/ContactForm";
+import { Link } from "react-router-dom";
 
 let LiveWebsites = () => {
   let [current, setCurrent] = useState(0);
   let images = [
-    "public\assets\imgs\Client work\Q1.JPG",
-    "/assets/imgs/Client work/CB.jpg",
-    "/assets/imgs/Client work/CB2.png",
-    "/assets/imgs/Client work/CB3.png",
-    "/assets/imgs/Client work/CB4.png",
-    "assets/imgs/Client work/Q-restaurant-page.JPG",
-    "https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://i.ibb.co/RTNydQ4/Q1.jpg",
+    "https://i.ibb.co/T0kWXQG/Q2.jpg",
+    "https://i.ibb.co/gVfMQHF/Q4.jpg",
+    "https://i.ibb.co/n75f9SM/Q7.jpg",
+    "https://i.ibb.co/znSgmBT/Q8.jpg",
+    "https://i.ibb.co/k1sbWty/Q-restaurant-page.jpg",
+    "https://i.ibb.co/Mph58Gg/CB.jpg",
+    "https://i.ibb.co/dPF5FZZ/CB1.png",
+    "https://i.ibb.co/N7y3bhs/CB3.png",
+    "https://i.ibb.co/qJQSSMQ/CB4.png",
   ];
 
   let nextImage = () =>
@@ -20,36 +26,33 @@ let LiveWebsites = () => {
   let jumpImage = (index) => setCurrent(index);
 
   return (
-    <div className="gallery-container">
-      <span className="button-prev" onClick={prevImage}>
-        chevron_left
-      </span>
-      <span className="button-next" onClick={nextImage}>
-        chevron_right
-      </span>
-      <div className="gallery-track">
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className="slide-image"
-            style={{
-              backgroundImage: `url(${image})`,
-              transform: `translateX(${(index - current) * 100}%)`,
-            }}
-          />
-        ))}
+    <div>
+      <Header />
+      <div className="back-link">
+        <Link to="/">Back</Link>
       </div>
-      <div className="gallery-footer">
-        {images.map((_, index) => (
-          <div
-            key={index}
-            className={`bubble-outer ${index === current ? "active" : ""}`}
-            onClick={() => jumpImage(index)}
-          >
-            <div className="bubble-inner" />
-          </div>
-        ))}
+      <div className="gallery-container">
+        <span className="button-prev" onClick={prevImage}>
+          chevron_left
+        </span>
+        <span className="button-next" onClick={nextImage}>
+          chevron_right
+        </span>
+        <div className="gallery-track">
+          {images.map((image, index) => (
+            <div
+              key={index}
+              className="slide-image"
+              style={{
+                backgroundImage: `url(${image})`,
+                transform: `translateX(${(index - current) * 100}%)`,
+              }}
+            />
+          ))}
+        </div>
       </div>
+      <ContactForm />
+      <Footer />
     </div>
   );
 };
