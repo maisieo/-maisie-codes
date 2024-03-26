799548499aeeaa1ea1ff1c2bf701c971eca90925
Maisie <maisieobb@googlemail.com>
2024-03-26 15:39:38 +0000

    test two

--- src/components/Home/ContactForm/ContactForm.js
+++ src/components/Home/ContactForm/ContactForm.js
@@ -140,6 +140,26 @@ function ContactForm() {
           <button type="submit">Send</button>
         </p>
       </form>
+
+
+
+      <form name="contact" method="post">
+      <p>
+        <label htmlFor="name">Name</label> <br />
+        <input type="text" id="name" name="name" required />
+      </p>
+      <p>
+        <label htmlFor="email">Email</label> <br />
+        <input type="email" id="email" name="email" required />
+      </p>
+      <p>
+        <label htmlFor="message">Message</label> <br />
+        <textarea id="message" name="message" required></textarea>
+      </p>
+      <p>
+        <input type="submit" value="Submit message" />
+      </p>
+    </form>
     </div>
   );
 }
