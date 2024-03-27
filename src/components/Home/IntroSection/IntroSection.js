f169fcf478b274c1095ca4713ae4bcdbeb8c8a58
Maisie <maisieobb@googlemail.com>
2024-03-27 09:39:14 +0000

    change padding to variables

--- src/components/Home/IntroSection/IntroSection.js
+++ src/components/Home/IntroSection/IntroSection.js
@@ -5,9 +5,9 @@ function IntroSection() {
   return (
     <section className="intro-section">
       <div className="text-content">
-        <h1 className="header-text">I'm <span className="underline">Maisie!</span><br />
+        <h1 className="header-text">Hi, I'm <span className="underline">Maisie!</span><br />
         A Frontend Developer</h1>
-        <p className="sub-heading">FE developer with experience building pixel-perfect front ends with Javascript libraries such as React, LWC, and with other technologies and languages like HTML, CSS, Figma, Litmus.</p>
+        <p className="sub-heading">A front-end developer with solid experience building pixel-perfect and responsive UI elements, applications and websites. Drop me a line if you think I can be of help!</p>
         <a className="contact-button" href='google.com'>Get in touch</a>
       </div>
       <div className="profile-img">
